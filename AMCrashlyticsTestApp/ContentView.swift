dee22efe78ef37b5ab6d7006dee2cedbe48a24a7
Andrew Marmion <andrew.marmion@gmail.com>
2020-06-10 17:40:08 +0100

    Add Firebase Crashlytics

--- AMCrashlyticsTestApp/ContentView.swift
+++ AMCrashlyticsTestApp/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            Text("Hello").font(.title)
+        }
     }
 }
 
